dbe7c8e57e7d63eded0970d810c2a5683f4936cf
MayankTravel <mayank.kumar@midastravel.org>
2023-10-11 10:25:00 +0530

    Added Eslist

--- components/Homebanner.js
+++ components/Homebanner.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Engine from "./Engine";
-// import Selection from "./Selection";
-
 const Homebanner = () => {
   return (
     <>
       <div className="container home-banner">
         <div className="row ">
           <div className="py-8 py-xl-10 pb-5 text-center  mb-5">
-            <h1 className="banner-heading">Let's The World Together!</h1>
+            <h1 className="banner-heading">Let&lsquo;s The World Together!</h1>
             <p className="banner-para text-white">
-              Find awesome hotel, tour, car and activities in London
+              Find awesome hotel&lsquo; tour&lsquo; car and activities in London
             </p>
           </div>
           <div className="book-flight-engine">
