c0e424bc9e3f46b3a5e0455eebdc463bd473b9cf
Mayank-Anonymously <craftaar0@gmail.com>
2023-11-05 11:03:46 +0530

    Changes

---
+++ pages/flightresult.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const flightresult = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="col-md-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default flightresult;
